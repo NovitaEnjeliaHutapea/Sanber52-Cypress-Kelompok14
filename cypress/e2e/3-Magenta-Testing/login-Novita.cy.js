008eec79660debee6197a06b7b55d825b86553b6
Novita <60680049+NovitaEnjeliaHutapea@users.noreply.github.com>
2024-01-02 14:23:13 +0700

    Update login-Novita.cy.js

--- cypress/e2e/3-Magenta-Testing/login-Novita.cy.js
+++ cypress/e2e/3-Magenta-Testing/login-Novita.cy.js
@@ -5,38 +5,35 @@ describe('Testing Website Magento', () => {
   
   it('Can Login with valid email and password?', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
-  
-   cy.get('input[type="email"]').type("belaika@gmail.com");
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("zKpKBMSVTqD*t2e");
-  
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
+    cy.get('input[type="email"]').type("belaika@gmail.com");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("zKpKBMSVTqD*t2e");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
   })
 
   it('Can Login with incorrect email/password?', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
-  
-   cy.get('input[type="email"]').type("novitahutapea28@gmail.com");
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("123456789");
-  
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
+    cy.get('input[type="email"]').type("belaika@gmail.com");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("123456789");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
+  })
+
+  it('Can Login with invalid email?', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
+    cy.get('input[type="email"]').type("belaika");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("123456789");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
   })
 
   it('Can Login with empty email?', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
-  
-   
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("123456789");
-  
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')   
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("123456789");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
   })
 
   it('Can Login with empty password?', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
-  
-    cy.get('input[type="email"]').type("novitahutapea28@gmail.com");
-   
-   
-   cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
+    cy.get('input[type="email"]').type("belaika@gmail.com");
+    cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
   })
   
 })
