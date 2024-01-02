116a3d4b489a5df7904386ae434420f636bc5a82
Novita <60680049+NovitaEnjeliaHutapea@users.noreply.github.com>
2024-01-02 14:57:31 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/',
-    viewportHeight: 680,
+    baseUrl: 'https://magento.softwaretestingboard.com',
+   
     defaultCommandTimeout:8000,
-    testIsolation: false,
+    
     
     setupNodeEvents(on, config) {
       // implement node event listeners here
