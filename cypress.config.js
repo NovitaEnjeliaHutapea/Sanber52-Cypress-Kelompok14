6067ba47bb69371a9e6899dfb396dab4ad42020d
Eka Marliana Puteri <ekamarlianaputeri@gmail.com>
2024-01-03 22:53:32 +0700

    resolve conflict

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: 'https://kasirdemo.belajarqa.com/',
-    supportFile: false,
-    //viewportWidth: 1800,
-    //viewportHeight: 2500,
-   
     setupNodeEvents(on, config) {
     },
   },
